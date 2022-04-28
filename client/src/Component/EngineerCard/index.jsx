ca1198241a59cc291e7a4e62702917a0a0856687
braaAwni <braaberoo95@gmail.com>
2022-04-28 23:37:42 +0300

    single card component

--- client/src/Component/EngineerCard/index.jsx
+++ client/src/Component/EngineerCard/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import SingleCard from './SingleCard'
 import './style.css'
 
 const EngCard = () => {
@@ -11,27 +12,9 @@ const EngCard = () => {
    <p>We Have A Team Of Amzining Engineers</p>
        </div>
  <div className='cardDetailes'>
- <div class="card">
-  <img src="http://themesflat.com/html/nah/images/member/1t1.png" alt="Avatar" />
-  <div className='Engdetailes'>
-    <h4><b>John Doe</b></h4> 
-    <p>Architect & Engineer</p> 
-  </div>
-</div>
-<div class="card">
-  <img src="http://themesflat.com/html/nah/images/member/1t1.png" alt="Avatar" />
-  <div className='Engdetailes'>
-    <h4><b>John Doe</b></h4> 
-    <p>Architect & Engineer</p> 
-  </div>
-</div>
-<div class="card">
-  <img src="http://themesflat.com/html/nah/images/member/1t1.png" alt="Avatar" />
-  <div className='Engdetailes'>
-    <h4><b>John Doe</b></h4> 
-    <p>Architect & Engineer</p> 
-  </div>
-</div>
+<SingleCard/>
+<SingleCard/>
+<SingleCard/>
  </div>
  <div className='cardBtn'>
  <button className='btn'>JOIN US !</button>
