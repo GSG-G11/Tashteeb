7beba08594e3c1b77ac6c108f7e60eb34f56cfd9
mo7amedehab97 <gazacityface@gmail.com>
2022-04-28 18:05:43 +0300

    cretae the navbar relates #5

--- client/src/Component/Header/Header.jsx
+++ client/src/Component/Header/Header.jsx
@@ -1,11 +1,11 @@
 import Navbar from '../Navbar/Navbar'
 const Header = () => {
   return (
-      <div className='header-holder'>
-           Header          
-          <Navbar />
+    <div className="header-holder">
+      <Navbar />
+      Header
     </div>
-  )
+  );
 }
 
 export default Header
--- client/src/Component/Navbar/Navbar.jsx
+++ client/src/Component/Navbar/Navbar.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
-
+import './Navbar.css'
+import logo from '../../Assets/Group 91.svg'
 const Navbar = () => {
   return (
-    <div className='navbar-holder'>Navbar</div>
+      <nav className='navbar-holder'>
+          <section className='app-logo'>
+              <img src={logo} alt="website logo"  />
+          </section>
+          <section className='navbar-info'>
+              <a>Home</a>
+              <a>Shop </a>
+              <a>Services</a>
+          </section>
+          <section className='auth-section'>
+              <button className="auth-btn signin-btn">Sign In</button>
+              <button className="auth-btn signup-btn">Sign Up</button>
+          </section>
+    </nav>
   )
 }
 
