a75be04efedcd61f0dec55d41354128779a0d7f0
mo7amedehab97 <gazacityface@gmail.com>
2022-04-28 17:17:19 +0300

    initialize the header and navbar files relates #5

---
+++ client/src/Component/Header/Header.jsx
@@ -0,0 +1,11 @@
+import Navbar from '../Navbar/Navbar'
+const Header = () => {
+  return (
+      <div className='header-holder'>
+           Header          
+          <Navbar />
+    </div>
+  )
+}
+
+export default Header
