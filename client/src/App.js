774c1d4a22af8c0f5fc019f674d1af09f2889e78
mo7amedehab97 <gazacityface@gmail.com>
2022-04-29 03:09:33 +0300

    solve conflict

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,26 @@
 import Header from './Component/Header/Header';
 import ServiceHolder from './Component/Services/ServiceHolder';
+import Header from './Component/Header'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
+import NotFoundPage from './Pages/NotFound'
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <ServiceHolder />
+     
+      <Router>
+        <Routes>
+          <Header />
+          <Route path="/" element={<>
+            <Header />
+            <ServiceHolder />
+          </>} >
+          <Route path="/*" element={<NotFoundPage />} />
+        </Routes>
+      </Router>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ server/src/database/models/HiringOrder.ts
@@ -0,0 +1,34 @@
+import { Model, DataTypes } from 'sequelize';
+import sequelize from '../config/connection';
+
+interface IHiringOrder extends Model {
+  id: number;
+  userId: number;
+  engId: number;
+  status: string;
+}
+
+const HiringOrder = sequelize.define<IHiringOrder>(
+  'hiringOrder',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    engId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    status: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+);
+
+export default HiringOrder;
