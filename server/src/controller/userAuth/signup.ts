6270fabd8d4c95ee5045a9659fbf21d25974ebf5
mo7amedehab97 <gazacityface@gmail.com>
2022-05-02 04:24:23 +0300

    create access token and create sign jwt

--- server/src/controller/userAuth/signup.ts
+++ server/src/controller/userAuth/signup.ts
@@ -1,9 +1,18 @@
 import { Request, Response } from 'express';
+import bcrypt from 'bcrypt';
+import { sign, Secret } from 'jsonwebtoken';
+import dotenv from 'dotenv';
 import { User } from '../../database';
 
+dotenv.config();
+
+const { SECRET_KEY } = process.env;
 const signUp = async (req: Request, res: Response) => {
+  const {
+    password, email, username,
+  } = req.body;
   const emailDoesExist = await User.findOne({
-    where: { email: req.body.email },
+    where: { email },
   });
 
   if (emailDoesExist) {
@@ -11,14 +20,25 @@ const signUp = async (req: Request, res: Response) => {
   }
 
   const userNameExists = await User.findOne({
-    where: { username: req.body.username },
+    where: { username },
   });
 
   if (userNameExists) {
     res.status(409).json({ message: 'This user name is already taken!' });
   }
+  const hashedPassword = bcrypt.hash(password, 10);
+  const user = await User.create({
+    ...req.body,
+    password: hashedPassword,
+  });
+  res.status(201).json({ message: 'User created successfully!' });
 
-  res.status(200).json({ message: 'succsesfull' });
+  const token = sign(
+    { id: user.id, username: user.username },
+    SECRET_KEY as Secret,
+    { expiresIn: '10h' },
+  );
+  res.cookie('token', token, { httpOnly: true }).json({ message: 'User created successfully!', user: { id: user.id, username: user.username } });
 };
 
 export default signUp;
