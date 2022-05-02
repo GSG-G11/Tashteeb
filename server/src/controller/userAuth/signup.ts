5e1f6758c6bdb14754a0aecf9156addd0f97e86b
KAHMOOSHA <brhmhelou@gmail.com>
2022-05-02 03:12:41 +0300

    check if email or username already exists or not

--- server/src/controller/userAuth/signup.ts
+++ server/src/controller/userAuth/signup.ts
@@ -1,6 +1,23 @@
 import { Request, Response } from 'express';
+import { User } from '../../database';
 
-const signUp = (req: Request, res: Response) => {
+const signUp = async (req: Request, res: Response) => {
+  const emailDoesExist = await User.findOne({
+    where: { email: req.body.email },
+  });
+
+  if (emailDoesExist) {
+    res.status(409).json({ message: 'This email is already taken!' });
+  }
+
+  const userNameExists = await User.findOne({
+    where: { username: req.body.username },
+  });
+
+  if (userNameExists) {
+    res.status(409).json({ message: 'This user name is already taken!' });
+  }
+
   res.status(200).json({ message: 'succsesfull' });
 };
 
