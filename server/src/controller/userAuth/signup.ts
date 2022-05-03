236c48e1adc13b836ad748f0d9bb2c4d40a7440d
KAHMOOSHA <brhmhelou@gmail.com>
2022-05-04 01:05:18 +0300

    Handled errors for signup

--- server/src/controller/userAuth/signup.ts
+++ server/src/controller/userAuth/signup.ts
@@ -3,40 +3,47 @@ import bcrypt from 'bcrypt';
 import { sign, Secret } from 'jsonwebtoken';
 import dotenv from 'dotenv';
 import { User } from '../../database';
+import CustomizeError from '../../error/customizeError';
+import handleKnownExceptions from '../../error/handleKnownError';
 
 dotenv.config();
 
 const { SECRET_KEY } = process.env;
+
 const signUp = async (req: Request, res: Response) => {
-  const {
-    password, email, username,
-  } = req.body;
-  const emailDoesExist = await User.findOne({
-    where: { email },
-  });
+  try {
+    const {
+      password, email, username,
+    } = req.body;
+    const emailDoesExist = await User.findOne({
+      where: { email },
+    });
 
-  if (emailDoesExist) {
-    res.status(409).json({ message: 'This email is already taken!' });
-  }
+    if (emailDoesExist) {
+      throw new CustomizeError(409, 'This email is already taken!');
+    }
 
-  const userNameExists = await User.findOne({
-    where: { username },
-  });
+    const userNameExists = await User.findOne({
+      where: { username },
+    });
 
-  if (userNameExists) {
-    res.status(409).json({ message: 'This user name is already taken!' });
-  }
-  const hashedPassword : string = await bcrypt.hash(password, 10);
-  const user = await User.create({
-    ...req.body,
-    password: hashedPassword,
-  });
-  const token = sign(
-    { id: user.id, username: user.username },
+    if (userNameExists) {
+      throw new CustomizeError(409, 'This username is already taken!');
+    }
+    const hashedPassword : string = await bcrypt.hash(password, 10);
+    const user = await User.create({
+      ...req.body,
+      password: hashedPassword,
+    });
+    const token = sign(
+      { id: user.id, username: user.username },
     SECRET_KEY as Secret,
     { expiresIn: '10h' },
-  );
-  res.cookie('token', token, { httpOnly: true }).json({ message: 'User created successfully!', user: { id: user.id, username: user.username } });
+    );
+    res.cookie('token', token, { httpOnly: true }).json({ message: 'User created successfully!', user: { id: user.id, username: user.username } });
+  } catch (err) {
+    handleKnownExceptions(err, res);
+  }
 };
 
 export default signUp;
