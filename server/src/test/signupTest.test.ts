2172ba5e2385620acb9e0a6761d41f769895f8ac
mo7amedehab97 <gazacityface@gmail.com>
2022-05-04 04:10:52 +0300

    add test for duplicate username and email

--- server/src/test/signupTest.test.ts
+++ server/src/test/signupTest.test.ts
@@ -27,6 +27,53 @@ describe('POST /signup', () => {
       });
   });
 });
+
+// test for duplicate email
+describe('POST /signup', () => {
+  test('duplicate email', (done) => {
+    supertest(app)
+      .post('/signup')
+      .send({
+        username: 'hallos',
+        password: 'password',
+        email: 'mohamed@gmail.com',
+        role: 1,
+        phone: '0594121212',
+      })
+      .expect(409)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.status).toBe(409);
+        expect(JSON.parse(res.text).error.message).toBe('This email is already taken!');
+
+        return done();
+      });
+  });
+});
+
+// test for duplicate username
+describe('POST /signup', () => {
+  test('duplicate username', (done) => {
+    supertest(app)
+      .post('/signup')
+      .send({
+        username: 'mohamed',
+        password: 'password',
+        email: 'helles@gmail.com',
+        role: 1,
+        phone: '0594121212',
+      })
+      .expect(409)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.status).toBe(409);
+        expect(JSON.parse(res.text).error.message).toBe('This username is already taken!');
+
+        return done();
+      });
+  });
+});
+
 afterAll(() => {
   sequelize.close();
 });
