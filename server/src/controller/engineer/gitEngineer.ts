e108d312bf10ff97e23a81c64873499dc3bbb528
braaAwni <braaberoo95@gmail.com>
2022-05-05 18:41:54 +0300

    build: get user home page backend

---
+++ server/src/controller/engineer/gitEngineer.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+import { User } from '../../database';
+import handleUnknownExceptions from '../../error/handleUnkownError';
+
+const getEngineer = async (req : Request, res : Response) => {
+  try {
+    const users = await User.findAll({ limit: 3 });
+    res.json({ status: 200, data: users });
+  } catch (err: any) {
+    handleUnknownExceptions(err, res);
+  }
+};
+
+export default getEngineer;
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import signUp from '../controller/userAuth';
+import getEngineer from '../controller';
 
 const router = Router();
 
 router.post('/signup', signUp);
+router.get('/engHome', getEngineer);
 
 export default router;
