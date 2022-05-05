f31d0990a98ebbc999cd5476bfa8bdcd836be8cb
mo7amedehab97 <gazacityface@gmail.com>
2022-05-05 16:51:26 +0300

    feat: add log out controller to index and import the controller

--- server/src/controller/userAuth/logout.ts
+++ server/src/controller/userAuth/logout.ts
@@ -1,8 +1,7 @@
-// eslint-disable-next-line import/no-import-module-exports
 import { Request, Response } from 'express';
 
 const logout = (req: Request, res: Response) => {
   res.clearCookie('token').json({ message: 'logged out successfully' });
 };
 
-module.exports = { logout };
+export default logout;
