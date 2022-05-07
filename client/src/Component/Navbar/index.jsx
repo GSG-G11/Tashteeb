45c3790e549336cfab396f322142febc30fcda1c
mo7amedehab97 <gazacityface@gmail.com>
2022-05-07 19:43:32 +0300

    feat: add user info component and add condition if user signd up show the user info component

--- client/src/Component/Navbar/index.jsx
+++ client/src/Component/Navbar/index.jsx
@@ -4,8 +4,11 @@ import { Button } from 'antd';
 import { Link } from 'react-router-dom';
 import logo from '../../Assets/Group 91.svg';
 import SignupModal from '../Modal';
+import UserInfo from './UserInfo';
+import { useAuth } from '../../Context/AuthContext';
 
 function Navbar() {
+  const { user } = useAuth();
   const [navbar, setNavbar] = useState('transparent');
   const changeBackground = () => {
     if (window.scrollY > 50) {
@@ -34,26 +37,32 @@ function Navbar() {
           marginBottom: '25px',
         }}
       >
-        <Button
-          type="primary"
-          ghost
-          style={{
-            marginTop: '25px',
-            backgroundColor: 'transparent',
-            borderRadius: ' 5px',
-            border: ' 1px solid #EDB820',
-            color: '#EDB820',
-            fontWeight: ' 500',
-            transition: 'all 1s ease-in-out',
-            display: 'flex',
-            alignItems: 'center',
-            marginRight: '10px',
-          }}
-          className="signin-btn"
-        >
-          Sign In
-        </Button>
-        <SignupModal />
+        {!user.user ? (
+          <>
+            <Button
+              type="primary"
+              ghost
+              style={{
+                marginTop: '25px',
+                backgroundColor: 'transparent',
+                borderRadius: ' 5px',
+                border: ' 1px solid #EDB820',
+                color: '#EDB820',
+                fontWeight: ' 500',
+                transition: 'all 1s ease-in-out',
+                display: 'flex',
+                alignItems: 'center',
+                marginRight: '10px',
+              }}
+              className="signin-btn"
+            >
+              Sign In
+            </Button>
+            <SignupModal />
+          </>
+        ) : (
+          <UserInfo username={user.user.username} />
+        )}
       </section>
     </nav>
   );
