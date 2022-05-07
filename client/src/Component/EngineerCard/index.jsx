cd322bffa3cc10be05d6c95675d1bf422b0e578e
braaAwni <braaberoo95@gmail.com>
2022-05-07 22:31:24 +0300

    fix: edit engineer controller

--- client/src/Component/EngineerCard/index.jsx
+++ client/src/Component/EngineerCard/index.jsx
@@ -3,27 +3,26 @@ import SingleCard from './SingleCard';
 import './style.css';
 
 function EngCard() {
-  const [info] = useState('');
+  const [info, setInfo] = useState([]);
   useEffect(() => {
     const abortController = new AbortController();
     fetch('/api/engHome', {
       signal: abortController.signal,
     })
       .then((res) => res.json())
-      .then((data) => console.log(data))
+      .then((data) => setInfo(data.data))
       .catch((err) => console.log(err));
     return () => abortController.abort();
-  }, [info]);
+  }, []);
   return (
+    // {id: 1, email: 'admin@admin.com', username: 'admin', password: 'admin', image: '
     <div className="containerCard">
       <div className="EngCardTitle">
         <h2>OUR TEAM</h2>
         <p>We Have A Team Of Amzining Engineers</p>
       </div>
       <div className="cardDetailes">
-        <SingleCard />
-        <SingleCard />
-        <SingleCard />
+        {info.map((item) => <SingleCard key={item.id} img={item.image} name={item.username} />)}
       </div>
       <div className="cardBtn">
         <button type="button" className="btn">
--- server/src/controller/engineer/gitEngineer.ts
+++ server/src/controller/engineer/gitEngineer.ts
@@ -4,7 +4,7 @@ import handleUnknownExceptions from '../../error/handleUnkownError';
 
 const getEngineer = async (req : Request, res : Response) => {
   try {
-    const users = await User.findAll({ limit: 3 });
+    const users = await User.findAll({ limit: 3, order: [['createdAt', 'DESC']] });
     res.json({ status: 200, data: users });
   } catch (err: any) {
     handleUnknownExceptions(err, res);
