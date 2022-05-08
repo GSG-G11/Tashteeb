05ecce55164b8ee58f701ccbf47869d0daf7f23d
mo7amedehab97 <gazacityface@gmail.com>
2022-05-08 23:53:32 +0300

    feat: create dashboard components related #46

--- client/src/Component/Dashboard/index.jsx
+++ client/src/Component/Dashboard/index.jsx
@@ -1,39 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
-import {
-  Layout, Menu, Breadcrumb,
-} from 'antd';
-import {
-  UserOutlined,
-  LaptopOutlined,
-} from '@ant-design/icons';
+import { Layout, Menu, Breadcrumb } from 'antd';
+import { UserOutlined, LaptopOutlined } from '@ant-design/icons';
 import { Link } from 'react-router-dom';
 import logo from '../../Assets/Group 91.svg';
+import ProductList from './ProductList';
+import Satistics from './Satistics';
 
-const {
-  Header, Sider, Content,
-} = Layout;
-const items2 = [UserOutlined, LaptopOutlined].map(
-  (icon, index) => {
-    const key = String(index + 1);
-    return {
-      key: `sub${key}`,
-      icon: React.createElement(icon),
-      label: `naav ${key}`,
-    };
-  },
-);
+const { Header, Sider, Content } = Layout;
+const items2 = [UserOutlined, LaptopOutlined].map((icon, index) => {
+  const key = String(index + 1);
+  return {
+    key: `sub${key}`,
+    icon: React.createElement(icon),
+    label: ` ${key === '1' ? 'Products' : 'Satistics'}`,
+  };
+});
 function Dashboard() {
+  const [subMenu, setSubMenu] = useState(false);
   return (
     <div className="Dashboard">
       <Layout>
-        <Header style={{ padding: 10 }} className="Dashboard-header">
+        <Header
+          style={{ padding: 10, backgroundColor: '#1D202F' }}
+          className="Dashboard-header"
+        >
           <Link to="/">
             <section className="app-logo">
               <img src={logo} alt="website logo" />
             </section>
           </Link>
-          {' '}
           <h2>IBRAHIM</h2>
         </Header>
         <Layout>
@@ -41,12 +37,15 @@ function Dashboard() {
             <Menu
               theme="dark"
               mode="inline"
-              defaultSelectedKeys={['1']}
+              defaultSelectedKeys={['2']}
               defaultOpenKeys={['sub1']}
               style={{
                 height: '100%',
+                paddingTop: '50px',
+                backgroundColor: '#1D202F',
               }}
               items={items2}
+              onClick={() => setSubMenu(!subMenu)}
             />
           </Sider>
           <Layout>
@@ -55,8 +54,14 @@ function Dashboard() {
                 <Breadcrumb.Item>Home</Breadcrumb.Item>
                 <Breadcrumb.Item>Dashboard</Breadcrumb.Item>
               </Breadcrumb>
-              <div style={{ background: '#fff', padding: 24, minHeight: 450 }}>
-                <h2>hello hleo</h2>
+              <div
+                style={{
+                  background: '#fff',
+                  padding: 24,
+                  minHeight: 450,
+                }}
+              >
+                {subMenu ? <ProductList subMenu={subMenu} /> : <Satistics />}
               </div>
             </Content>
           </Layout>
