873145d7d5c72585234f28581ac4e2d10e98bbb5
braaAwni <braaberoo95@gmail.com>
2022-05-08 18:22:03 +0300

    build: engineer page front

--- client/src/Component/Engineer/index.jsx
+++ client/src/Component/Engineer/index.jsx
@@ -1,19 +1,25 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import SingleCard from '../EngineerCard/SingleCard';
+import '../EngineerCard/style.css';
 import './style.css';
 
-function Engineer() {
-//   const [info, setInfo] = useState([]);
+function EngineerInfo() {
+  const [info, setInfo] = useState([]);
   useEffect(() => {
-    const controller = new AbortController();
-    fetch('/api/engPage', { signal: controller.signal }).then((res) => res.json()).then((data) => console.log(data.data)).catch((err) => console.log(err));
-    return () => controller.abort();
+    const abortController = new AbortController();
+    fetch('/api/engPage', {
+      signal: abortController.signal,
+    })
+      .then((res) => res.json())
+      .then((data) => setInfo(data.data.rows))
+      .catch((err) => console.log(err));
+    return () => abortController.abort();
   }, []);
   return (
-    <div className="engineerPage">
-      <p> hello </p>
-      <p> hello </p>
+    <div className="cardDetailes engineerInfo">
+      {info.map((item) => <SingleCard key={item.id} name={item.username} img={item.image} />)}
     </div>
   );
 }
 
-export default Engineer;
+export default EngineerInfo;
---
+++ client/src/Pages/EngineerPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import EngineerInfo from '../Component/Engineer';
+
+function EngineerPage() {
+  return (
+    <div>
+      <EngineerInfo />
+    </div>
+  );
+}
+
+export default EngineerPage;
