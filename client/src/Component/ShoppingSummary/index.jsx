9bfa406ea778d21a694b61bcdb1062583e758cf3
nabilramy <nabil.abushaban@gmail.com>
2022-05-14 23:55:10 +0300

    make cart functionalities and alerts

--- client/src/Component/ShoppingSummary/index.jsx
+++ client/src/Component/ShoppingSummary/index.jsx
@@ -1,52 +1,96 @@
+/* eslint-disable max-len */
 /* eslint-disable react/jsx-one-expression-per-line */
 import React, { useContext } from 'react';
+import { Modal } from 'antd';
 import axios from 'axios';
 import './style.css';
 import { Context } from '../../Context/shoppingSummary';
+import { user } from '../../Context/AuthContext';
+
+const ReachableContext = React.createContext();
+
+const empty = {
+  title: 'Cart is empty!',
+  content: (
+    <ReachableContext.Consumer>Please add some items to your cart before checkouting.</ReachableContext.Consumer>
+
+  ),
+};
+const notLoggedIn = {
+  title: 'Not Logged in!',
+  content: (
+    <ReachableContext.Consumer>Please log in to checkout</ReachableContext.Consumer>
+  ),
+};
 
 function ShoppingSummary() {
   const { totalPrice, cart, setCart } = useContext(Context);
-  const checkout = async () => {
-    await axios.post('/checkout', {
-      products: cart,
-    });
-    setCart([]);
+  const confirmCheckout = {
+    title: 'Confirm Purchasing!',
+    content: (
+      <ReachableContext.Consumer>Are you sure about these items?</ReachableContext.Consumer>
+    ),
+    okText: 'Yes',
+    cancelText: 'No',
+    onOk: async () => {
+      const data = {
+        products: cart,
+      };
+      const response = await axios.post('/api/checkout', data);
+      if (response.data.success) {
+        setCart([]);
+      }
+    },
+  };
+  const [modal, contextHolder] = Modal.useModal();
+  const checkout = () => {
+    if (cart.length === 0) {
+      modal.warning(empty);
+    } else if (!user) {
+      modal.warning(notLoggedIn);
+    } else {
+      modal.confirm(confirmCheckout);
+    }
   };
   return (
-    <div className="summary">
-      <div className="shopSummary">
-        <h3>Shopping Summary</h3>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
-          ut labore et dolore magna aliqua.
-        </p>
-        <span>Have a coupon code?</span>
-        <div className="promoCode">
-          <input type="text" placeholder="Enter param code here" />
-          <button type="button">
-            <i className="ri-arrow-right-line" />
-          </button>
+    <ReachableContext.Provider value="Light">
+      <div className="summary">
+        <div className="shopSummary">
+          <h3>Shopping Summary</h3>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+            ut labore et dolore magna aliqua.
+          </p>
+          <span>Have a coupon code?</span>
+          <div className="promoCode">
+            <input type="text" placeholder="Enter param code here" />
+            <button type="button">
+              <i className="ri-arrow-right-line" />
+            </button>
+          </div>
         </div>
-      </div>
-      <div className="buy">
-        <div className="subtotal">
-          <span>Subtotal</span>
-          <span className="num">${totalPrice}</span>
+        <div className="buy">
+          <div className="subtotal">
+            <span>Subtotal</span>
+            <span className="num">${totalPrice}</span>
+          </div>
+          <div className="tax">
+            <span>Tax</span>
+            <span className="num">%25</span>
+          </div>
+          <hr />
+          <div className="total">
+            <span>Total</span>
+            <span className="num">${totalPrice * 1.25}</span>
+          </div>
+          <button type="button" onClick={checkout} className="checkout-btn">CHECKOUT</button>
+          <span className="confirm">Confirm Shopping</span>
         </div>
-        <div className="tax">
-          <span>Tax</span>
-          <span className="num">%25</span>
-        </div>
-        <hr />
-        <div className="total">
-          <span>Total</span>
-          <span className="num">${totalPrice * 1.25}</span>
-        </div>
-        <button type="button" onClick={checkout} className="checkout-btn">CHECKOUT</button>
-        <span className="confirm">Confirm Shopping</span>
-      </div>
 
-    </div>
+      </div>
+      {contextHolder}
+
+    </ReachableContext.Provider>
   );
 }
 
--- client/src/Pages/Cart/index.jsx
+++ client/src/Pages/Cart/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button } from 'antd';
 import { ShoppingOutlined } from '@ant-design/icons';
+import { Link } from 'react-router-dom';
 import Table from './Table';
 import './style.css';
 import ShoppingSummary from '../../Component/ShoppingSummary';
@@ -11,9 +12,11 @@ export default function Cart() {
     <CartProvider>
       <div className="cart">
         <Table />
-        <Button className="continue-shopping" type="primary" shape="round" icon={<ShoppingOutlined />} size="large">
-          Continue Shopping
-        </Button>
+        <Link to="/">
+          <Button className="continue-shopping" type="primary" shape="round" icon={<ShoppingOutlined />} size="large">
+            Continue Shopping
+          </Button>
+        </Link>
         <ShoppingSummary />
       </div>
     </CartProvider>
