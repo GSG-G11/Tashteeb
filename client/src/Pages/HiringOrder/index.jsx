8ef33b4750ce8dfaab0280d0c9cf3c8f1a8b97e1
nabilramy <nabil.abushaban@gmail.com>
2022-05-23 15:22:52 +0300

    fix: styling

--- client/src/Pages/HiringOrder/index.jsx
+++ client/src/Pages/HiringOrder/index.jsx
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import { Typography } from 'antd';
+
 import axios from 'axios';
+import Navbar from '../../Component/Navbar';
+// import Footer from '../../Component/footer/Footer';
 import { useAuth } from '../../Context/AuthContext';
 import OrderCard from '../../Component/OrderCard';
 import './style.css';
@@ -29,26 +33,34 @@ export default function HiringOrder() {
     return () => {};
   }, [user, rerender]);
 
+  const { Title } = Typography;
   return (
-    <div>
-      <div className="hiring-order-container">
-        { orders.map((item) => (
-          <OrderCard
-            key={item.id}
-            id={item.id}
-            status={item.status}
-            details={item.description}
-            price={item.price}
-            time={item.createdAt}
-            reply={item.reply}
-            name={user.role === 1 ? item.userHiringOrder.username : item.engHiringOrder.username}
-            isEngineer={user.role === 1}
-            setRerender={setRerender}
-            rerender={rerender}
-
-          />
-        ))}
+    <>
+      <div className="eng-navbar-section">
+        <Navbar transparent={false} />
       </div>
-    </div>
+      {' '}
+      <div className="hiring-order">
+        <Title level={2}>Hiring Orders</Title>
+        <div className="hiring-order-container">
+          { orders.map((item) => (
+            <OrderCard
+              key={item.id}
+              id={item.id}
+              status={item.status}
+              details={item.description}
+              price={item.price}
+              time={item.createdAt}
+              reply={item.reply}
+              name={user.role === 1 ? item.userHiringOrder.username : item.engHiringOrder.username}
+              isEngineer={user.role === 1}
+              setRerender={setRerender}
+              rerender={rerender}
+            />
+          ))}
+        </div>
+      </div>
+      {/* <Footer /> */}
+    </>
   );
 }
