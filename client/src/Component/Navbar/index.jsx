d06319e135571c8785625c6a7daa92fbe3f1ddbb
nabilramy <nabil.abushaban@gmail.com>
2022-05-27 15:01:18 +0300

    add socket client

--- client/src/Component/Navbar/index.jsx
+++ client/src/Component/Navbar/index.jsx
@@ -1,7 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.css';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import { notification } from 'antd';
 import logo from '../../Assets/Group 91.svg';
 import SignupModal from '../Modal';
 import UserInfo from './UserInfo';
@@ -10,10 +11,12 @@ import {
 } from '../../Context/AuthContext';
 import LoginModal from '../Modal/login';
 import ShowBadge from '../Badge/Badge';
+import { useSocket } from '../../Context/useSocket';
 
 function Navbar({ transparent = true }) {
   const { user } = useAuth();
   const [navbar, setNavbar] = useState('transparent');
+
   const changeBackground = () => {
     if (window.scrollY > 50) {
       setNavbar('solid');
@@ -22,6 +25,30 @@ function Navbar({ transparent = true }) {
     }
   };
   window.addEventListener('scroll', changeBackground);
+
+  const openNotification = (data) => {
+    notification.open({
+      message: 'Order Created',
+      description: data,
+      onClick: () => {
+        console.log('Notification Clicked!');
+      },
+    });
+  };
+
+  const { socket } = useSocket();
+
+  useEffect(() => {
+    if (socket && user?.role === 2) {
+      socket.on('connect_error', (err) => {
+        console.log(`connect_error due to ${err.message}`);
+      });
+      socket.on('notification', (data) => {
+        openNotification(data.message);
+      });
+    }
+  }, []);
+
   return (
     <nav className={`navbar-holder ${transparent && navbar}`}>
       <section className="app-logo">
---
+++ client/src/Context/useSocket.jsx
@@ -0,0 +1,33 @@
+import React, {
+  createContext, useContext, useEffect, useMemo, useState,
+} from 'react';
+import PropTypes from 'prop-types';
+import io from 'socket.io-client';
+
+export const SocketContext = createContext();
+
+export function SocketProvider({ children }) {
+  const [socket, setSocket] = useState(null);
+
+  useEffect(() => {
+    const newSocket = io(`https://${window.location.hostname}`, {
+
+    });
+    setSocket(newSocket);
+    return () => newSocket.close();
+  }, []);
+
+  const value = useMemo(() => ({
+    socket,
+  }), [socket]);
+  return <SocketContext.Provider value={value}>{children}</SocketContext.Provider>;
+}
+
+SocketProvider.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node,
+  ]).isRequired,
+};
+
+export const useSocket = () => useContext(SocketContext);
