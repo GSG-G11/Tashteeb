414087c1a210c4e9bf2b496d2da009761e28ed75
nabilramy <nabil.abushaban@gmail.com>
2022-05-27 19:31:59 +0300

    add socket client

--- server/src/controller/cart/checkout.ts
+++ server/src/controller/cart/checkout.ts
@@ -1,7 +1,7 @@
 /* eslint-disable max-len */
 import { Request, Response } from 'express';
 import { Order, ProductOrder } from '../../database';
-import handleUnknownError from '../../error/handleUnkownError';
+import errorHandler from '../../error';
 
 interface IReqUser extends Request {
   user?: any;
@@ -33,6 +33,6 @@ export default async (req: IReqUser, res: Response) => {
       productOrder,
     });
   } catch (error: any) {
-    handleUnknownError(error, res);
+    errorHandler(error, res);
   }
 };
